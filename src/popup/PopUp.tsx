dd6b51e97364fc97c7b44fec73f68c408f8e4a68
Vivi <vithai1@berkeley.com>
2025-04-11 00:28:22 -0700

    Basic Extension Setup

---
+++ src/popup/PopUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PopUp: React.FC = () => {
+  return (
+    <div style={{ width: 300, padding: 20 }}>
+      <h1>Hello!</h1>
+      <p>This is your MVP popup 🎉</p>
+    </div>
+  );
+};
+
+export default PopUp;
