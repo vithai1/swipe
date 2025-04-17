5c99d69bf8e4dcbf961d8ee7221dbab4be0cb2d6
vithai1 <107958129+vithai1@users.noreply.github.com>
2025-04-17 14:03:15 -0700

    hard-code minimized log

--- src/popup/PopUp.tsx
+++ src/popup/PopUp.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import { MinimizedCard } from '../components/MinimizedCard';
 
 const PopUp: React.FC = () => {
   return (
-    <div style={{ width: 300, padding: 20 }}>
-      <h1>Hello!</h1>
-      <p>This is your MVP popup 🎉</p>
-    </div>
+    <MinimizedCard/>
   );
 };
 
